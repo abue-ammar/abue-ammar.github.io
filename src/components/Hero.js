85d87b28b380c2a464c94fdaff5fc6b3b6e1bcdc
developermithu <www.mithudas77@gmail.com>
2022-09-15 04:39:39 +0600

    added dark mode

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,31 @@
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 import sun from "../assets/icons/sun.svg";
 import profile from "../assets/profile.png";
 import { hero } from "../data";
 
 export default function Hero() {
-  const { theme, setTheme } = useTheme();
+  // const { theme, setTheme } = useTheme();
+  const [theme, setTheme] = useState(localStorage.theme);
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+
+    if (theme === "dark") {
+      root.classList.add("dark");
+    } else {
+      root.classList.remove("dark");
+    }
+
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const handleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
 
   return (
-    <div className="mb-20">
+    <div className="mb-20 dark:bg-dark dark:text-pink-500">
       <div className="flex flex-row items-center justify-between w-full">
         <div className="w-20 h-20 rounded-full relative overflow-hidden">
           <img src={profile} layout="fill" objectFit="contain" alt="Ammar" />
@@ -19,7 +37,7 @@ export default function Hero() {
           height={30}
           alt="Toggle theme"
           className="cursor-pointer toggleTheme"
-          onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+          onClick={handleTheme}
         />
       </div>
       <h1 className="mt-5 mb-4">{hero.title}</h1>
