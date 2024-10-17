c03d74c5c53c53bc77d028eff29af9fdac88baee
abue.ammar <abue.ammar@technonext.com>
2024-10-17 14:15:51 +0600

    skills done

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,20 +1,55 @@
 /* eslint-disable @next/next/no-img-element */
-import { skills } from "@/app/data";
-import React from "react";
+"use client";
+import { apiFetcher } from "@/utils/apiFetcher";
+import useSWR from "swr";
 import { Badge } from "./Badge";
 import Section from "./Section";
 
+interface ISkill {
+  name: string;
+  url: string;
+}
+
 interface SkillsProps {}
 
-const Skills: React.FC<SkillsProps> = (props) => {
+const Skills: React.FC<SkillsProps> = () => {
+  const { data, error } = useSWR<{ success: boolean; data: ISkill[] }>(
+    "/api/skills",
+    apiFetcher
+  );
+
+  const isLoading = !data && !error;
+
+  if (isLoading) {
+    return (
+      <div>
+        <Section title="Skills" />
+        <div className="flex flex-wrap gap-2">
+          {[...Array(7)].map((_, i) => (
+            <div
+              key={i}
+              className="bg-gray-200 h-6 w-20 rounded-md animate-pulse"
+            ></div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div>Error loading skills data: {error.message}</div>;
+  }
+
+  const skills = data?.data;
+
   return (
     <div className="">
-      <Section title={skills.title} />
-      <div className=" flex flex-wrap gap-2">
-        {skills?.skills?.map((skill, i) => (
+      <Section title="Skills" />
+      <div className="flex flex-wrap gap-2">
+        {skills?.map((skill, i) => (
           <Badge key={i}>
             <img
-              alt="Logo"
+              alt={`${skill?.name} Logo`}
               src={skill?.url}
               className="!mr-1"
               width="14"
