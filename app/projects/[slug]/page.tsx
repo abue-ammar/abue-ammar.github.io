1518bd779d504f1ceb90fe9d9cab22fbbd4f1620
abdarker <iabueammar@gmail.com>
2024-12-26 22:30:57 +0600

    link prefetch added

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,16 +1,18 @@
 import Link from "next/link";
-export async function generateStaticParams() {
-  const projects = await import("../../data/data").then((mod) => mod.projects);
+import { projects } from "../../data/data";
 
+export async function generateStaticParams() {
   return projects.map((project) => ({
     slug: project.slug,
   }));
 }
-type Params = Promise<{ slug: string }>;
 
-export async function generateMetadata({ params }: { params: Params }) {
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
   const { slug } = await params;
-  const projects = await import("../../data/data").then((mod) => mod.projects);
   const project = projects.find((p) => p.slug === slug);
 
   if (!project) {
@@ -26,9 +28,12 @@ export async function generateMetadata({ params }: { params: Params }) {
   };
 }
 
-export default async function ProjectPage({ params }: { params: Params }) {
+export default async function ProjectPage({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
   const { slug } = await params;
-  const projects = await import("../../data/data").then((mod) => mod.projects);
   const project = projects.find((p) => p.slug === slug);
 
   if (!project) {
@@ -43,8 +48,21 @@ export default async function ProjectPage({ params }: { params: Params }) {
   return (
     <article>
       <div className="mb-8 flex justify-between">
-        <Link href="/" className="">
-          Back To Home
+        <Link
+          href="/"
+          className="inline-flex items-center rounded-lg bg-neutral-100 px-2 py-1 font-[400] no-underline"
+          aria-label="home"
+        >
+          <svg
+            viewBox="0 0 24 24"
+            version="1.2"
+            baseProfile="tiny"
+            xmlns="http://www.w3.org/2000/svg"
+            className="mr-1 size-5"
+          >
+            <path d="M12 3s-6.186 5.34-9.643 8.232A1.04 1.04 0 0 0 2 12a1 1 0 0 0 1 1h2v7a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-4h4v4a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-7h2a1 1 0 0 0 1-1 .98.98 0 0 0-.383-.768C18.184 8.34 12 3 12 3" />
+          </svg>
+          Home
         </Link>
       </div>
       <div className="fade-in">
