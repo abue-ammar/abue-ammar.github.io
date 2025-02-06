67fa87b721793bf2c68376e472adb5a591e71e7b
Abue Ammar <darkerab@gmail.com>
2025-02-07 02:52:36 +0600

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 const Footer: React.FC = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="fade-in mt-10">
-      <p>Copyright &copy; 2024 Abue Ammar. All rights reserved.</p>
+      <p>Copyright &copy; {currentYear} Abue Ammar. All rights reserved.</p>
     </footer>
   );
 };
